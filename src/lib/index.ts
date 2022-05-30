ee5c7d0cce5a073aafc08970d4100639064ed5d2
parbez <imranbarbhuiya.fsd@gmail.com>
2022-05-30 15:16:00 +0530

    refactor!: simplify cancerous check

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,2 @@
-export * from './utils';
 export * from './decancer';
 export * from './isCancerous';
--- src/lib/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export const isAscii = (str: string) => /^[\x00-\x7F]*$/.test(str);
-export const isAlnum = (char: string) => /^[a-zA-Z0-9]$/.test(char);
