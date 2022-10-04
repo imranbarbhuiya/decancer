f87eff061bc87f587a7aac18e5c450cdf911d3ac
legendhimslef <sabdulrahman971@gmail.com>
2022-10-04 11:11:04 +0530

    feat: add ignoreSpace argument

--- src/lib/cancerPercentage.ts
+++ src/lib/cancerPercentage.ts
@@ -1,4 +1,5 @@
-export const cancerPercentage = (str: string) => {
+export const cancerPercentage = (str: string, ignoreSpace = false) => {
+	if (ignoreSpace) str = str.replace(/\s/g, '');
 	const cancerousChars = str.match(/[^\p{ASCII}]/gu);
 	const cancerousCharsCount = cancerousChars ? cancerousChars.length : 0;
 	return Math.round((cancerousCharsCount / str.length) * 100);
